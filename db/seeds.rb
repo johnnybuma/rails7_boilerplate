a12fe6fd8b17e45e64c96c53f868cdf8f5cb3122
Johnny <7820610+johnnybuma@users.noreply.github.com>
2024-10-10 20:27:30 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ include ActionView::Helpers::AssetUrlHelper
 #user = CreateAdminService.new.call
 #puts 'CREATED ADMIN USER: ' << user.email
 
-User.create(email: "admin@mail.com", username: "johnnyb", password: 'password', role: 2)
+User.create(email: "admin@mail.com", username: "Admin ", password: 'password', role: 2)
 User.create(email: "test@mail.com", username: "Test", password: 'password', role: 0)
 
 Room.create(name: 'Room One', is_private: false)
